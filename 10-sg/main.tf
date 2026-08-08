897fc028d8afe46b1e616837cd6ac0c4a1047751
Manibhargavi Bendalam <manibhargavibendalam@gmail.com>
2026-08-08 18:35:07 +0530

    intial commit

---
+++ 10-sg/main.tf
@@ -0,0 +1,20 @@
+#using open source module
+# module "catalogue" {
+#     source ="terraform-aws-modules/security-group/aws"
+
+#     use_name_prefix = false
+#     name="${local.common_name_suffix}-catalogue"
+#     description="Security group for catalogue with custom ports open within VPC,egress all traffic"
+#     vpc_id= data.aws_ssm_parameter.vpc_id.value
+# }
+
+
+module "sg" {
+    count = length(var.sg_names)
+    source = "git::https://github.com/Manibhargavibendalam/terraform-aws-sg.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = var.sg_names[count.index]
+    sg_description = "created for ${var.sg_names[count.index]}"
+    vpc_id =  local.vpc_id
+}

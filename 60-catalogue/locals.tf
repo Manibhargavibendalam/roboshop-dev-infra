5fff7746fd63ecaaec82fa71cea213a0ac1c4cfd
Manibhargavi Bendalam <manibhargavibendalam@gmail.com>
2026-08-17 18:00:23 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,6 @@
 locals{
     common_name_suffix = "${var.project_name}-${var.environment}"
-    private_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     ami_id=data.aws_ami.joindevops.id
     common_tags = {
         Project    = var.project_name

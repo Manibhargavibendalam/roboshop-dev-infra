1296cfa401c6b0bf1c8173a4f7c3322c8bf3aa8a
Manibhargavi Bendalam <manibhargavibendalam@gmail.com>
2026-08-17 19:43:56 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -84,3 +84,18 @@ resource "terraform_data" "catalogue" {
     ]
   }
 }
+
+
+#stop the instance to take image
+
+resource "aws_ec2_instance_state" "stop_my_instance"{
+    instance_id = aws_instance.my_instance.id
+    state = "stopped"
+    depends_on = [terraform_data.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${local.common_name_suffix}-catalogue-ami"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [aws_ec2_instance_state.catalogue] #we are this because stopping don't interrupt it..if we dont use this stopping will interrupt this
+}

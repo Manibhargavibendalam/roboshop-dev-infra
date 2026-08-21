9aa7822f292f93949c03979f11e0496b41533e83
Manibhargavi Bendalam <manibhargavibendalam@gmail.com>
2026-08-21 12:23:25 +0530

    updated

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,6 +1,6 @@
  resource "aws_lb" "backend_alb" {
   name               = "${local.common_name_suffix}-backend-alb" #roboshop-dev-backend-alb
-  internal           = true
+  internal           = false
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
